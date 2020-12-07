62d41a65871a90bb23afaa3465d6614a286057de
Sunil Mukta <sm18139@corp.ojas-it.com>
2020-12-07 12:31:07 +0530

    added

---
+++ src/main/java/com/uk/project/hystrix/HystrixApplication.java
@@ -0,0 +1,18 @@
+package com.uk.project.hystrix;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableEurekaClient
+public class HystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uk/project/hystrix/HystrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uk.project.hystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
